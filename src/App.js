8e0d505ba2183892f25f53c6fe123a59c1d93f06
Heli Kulomaa <heli.kulomaa@gmail.com>
2022-12-21 20:47:03 +0200

    -

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DO BY
+          DO BY 
         </p>
         <p>Tehtävälistasovellus</p>
       </header>
